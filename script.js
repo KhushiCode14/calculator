0f9175e50b05d77d7e871b42fc50e31a6c2087fc
khushi kumari <khushikumarigupta463@gmail.com>
2025-02-14 12:30:53 +0530

    Add basic calculator implementation with HTML, CSS, and JavaScript

---
+++ script.js
@@ -0,0 +1,32 @@
+// s;ecting element
+const input = document.getElementById("display"); // input
+const buttons = document.getElementsByClassName("button"); //button
+let expression = "";
+// console.log(buttons, input);
+// Array.from(buttons).forEach((button) => { ... }); --> convert HTMLCollection to an Array
+Array.from(buttons).forEach((button) => {
+  button.addEventListener("click", (e) => {
+    const value = e.target.innerHTML;
+    console.log(value);
+    if (value === "=") {
+      try {
+        expression = eval(expression); // Evaluate the expression
+        console.log(expression);
+        input.value = expression;
+        console.log(expression);
+      } catch {
+        input.value = "Error"; // Handle any errors
+      }
+    } else if (value === "C") {
+      expression = ""; // Clear the expression
+      input.value = expression;
+    } else {
+      expression += value; // Append the value to the expression
+      input.value = expression; // Update the display
+    }
+  });
+});
+console.log(expression);
+// alternative of eval is json.parse()
+
+
